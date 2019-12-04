99ebd9508ab5fa1bf2c273a2cd061157d326b266
michiyo.mikami <michiyo.mikami333@gmail.com>
2019-12-04 08:32:52 +0000

    user login

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,12 @@ Route::get('tasks', 'TasksController@index');
 Route::get('tasks/create', 'TasksController@create');
 Route::put('tasks/{id}', 'TasksController@update');
 */
+
+// ユーザ登録
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
+// ログイン認証
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
