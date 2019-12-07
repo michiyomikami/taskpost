6dd78eb2be9798a3733e5a921066b0cd89a0fa04
michiyo.mikami <michiyo.mikami333@gmail.com>
2019-12-07 11:00:21 +0000

    middleware

--- database/migrations/2019_12_04_215923_add_user_id_to_tasks_table.php
+++ database/migrations/2019_12_04_215923_add_user_id_to_tasks_table.php
@@ -15,7 +15,11 @@ class AddUserIdToTasksTable extends Migration
     {
         Schema::table('tasks', function (Blueprint $table) {
             $table->integer('user_id')->unsigned()->index();
+            
+            //　外部キー制約
+            $table->foreign('user_id')->references('id')->on('users');
         });
+        
     }
 
     /**
